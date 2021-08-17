8cc660165f0d11fe2ac2d7b3099c6066bbaea8a2
Haneen-97 <haneen-97@outlook.com>
2021-08-17 14:36:41 +0300

    code refactor

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-// TODO: Create a function to add books
-addBook()
+// function to add books
 function addBook(){
-
     $("#form-n").on("submit", (e) => {
         e.preventDefault()
-    axios({
-        method: 'POST',
-        url: 'https://61067a051f3487001743792a.mockapi.io/api/v1/Books',
-        data: {
-            title: $("#title").val(),
-            subtitle: $("#subtitle").val(),
-            author: $("#author").val(),
-            image: $("#image").val(),
+        axios({
+            method: 'POST',
+            url: 'https://61067a051f3487001743792a.mockapi.io/api/v1/Books',
+            data: {
+                title: $("#title").val(),
+                subtitle: $("#subtitle").val(),
+                author: $("#author").val(),
+                image: $("#image").val(),
         }
       })
         .then( res => {
@@ -25,35 +23,42 @@ function addBook(){
     })
 }
 
-// TODO: Create a function to view all books 
-
-//function call
-getAllBooks();
-
+// function to view all books 
 function getAllBooks(){
-
 axios({
     url: 'https://61067a051f3487001743792a.mockapi.io/api/v1/Books',
     method: 'get',
   })
   
   .then((response) => {    
-    if(response.data){
+    if(response.data.length>0){
+        $('#table').append(`
+        <thead >
+        <tr>
+         <th scope="col">#NO</th>
+          <th scope="col">Title</th>
+          <th scope="col">Subtitle</th>
+          <th scope="col">Author</th>
+          <th scope="col">Image</th>
+        <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody id="bookList"></tbody>`
+      )
         for (i in response.data) {  
             bookListData(response.data[i])
         }
     }else{
-        $('#bookList').append(` 
-      nothing to show`);
+        $('#table').append(`<h1 align="center">Nothing to show, please add a book</h1>`);
     }
   }) 
   .catch((error)=>{
-     
       console.log(error)
   }) 
 }
+// function to show book info in a row
   function bookListData(book){
-    $('#bookList').append(` 
+    $('#bookList').append(`
     <tr>
         <th scope='row'>${++i}</th>
         <td>${book.title}</td>
@@ -61,10 +66,12 @@ axios({
         <td>${book.author}</td>
         <td><img src="${book.image}" width="100" height="100"></td>
         <td><button type="button" class="btn btn-danger" onclick="deleteBook(${book.id})">Delete</button></td> 
-    </tr>`);
+    </tr>
+     `);
   }
 
-// TODO: Create a function to delete a book 
+
+// function to delete a book 
 function deleteBook(id){
     axios({
         url: 'https://61067a051f3487001743792a.mockapi.io/api/v1/Books/'+id,
@@ -73,9 +80,12 @@ function deleteBook(id){
       .then((response) => {
          window.location.reload(); 
          alert(`book ${id} deleted successfully`)
-       
       }) 
       .catch((error)=>{
           console.log(error)
       }) 
 }
+
+// function call
+addBook()
+getAllBooks()
